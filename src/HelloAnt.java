0803f7a3b686ba6e59645c172c3a8bdf13c52706
jianjun.bao <jianjun.bao@ecarx.com.cn>
2019-12-21 10:45:37 +0800

    hello world for ant send mail

---
+++ src/HelloAnt.java
@@ -0,0 +1,14 @@
+package com.zdz.ant.test;
+
+/**
+ * java ant test
+ * @author zheng
+ *
+ */
+public class HelloAnt {
+
+    public static void main(String[] args) {
+        System.out.println("Hello ant,by zdz!");
+    }
+
+}
